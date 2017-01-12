33ae7065d44324b619b96ba32d121510367dd88b
uraway <masato.u0107@gmail.com>
2017-01-12 16:22:50 +0900

    fix TwitterTweetButton

--- src/TwitterTweetButton.jsx
+++ src/TwitterTweetButton.jsx
@@ -1,5 +1,4 @@
-import React, { PropTypes } from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react';
 
 export default class TwitterTweetButton extends React.Component {
   constructor(props) {
@@ -13,14 +12,14 @@ export default class TwitterTweetButton extends React.Component {
     }
 
     if (typeof twttr === 'undefined') {
-      const twitterbutton = ReactDOM.findDOMNode(this.refs.twitterbutton);
+      const twitterbutton = this.node;
       const twitterscript = document.createElement('script');
       twitterscript.src = '//platform.twitter.com/widgets.js';
       twitterscript.async = true;
       twitterscript.id = 'twitter-wjs';
       twitterbutton.parentNode.appendChild(twitterscript);
     } else {
-      twttr.widgets.load();
+      twttr.widgets.load(); // eslint-disable-line
     }
 
     this.initialized();
@@ -30,23 +29,16 @@ export default class TwitterTweetButton extends React.Component {
     this.setState({ initialized: true });
   }
 
-  static PropTypes = {
-    url: PropTypes.string,
-    text: PropTypes.string,
-    via: PropTypes.string,
-    hashtags: PropTypes.string,
-  };
-
   render() {
     return (
       <a
-        ref="twitterbutton"
+        ref={node => this.node = node}
         href="https://twitter.com/share"
         className="twitter-share-button"
         data-url={this.props.url}
         data-text={this.props.text}
         data-via={this.props.user}
-        data-hashtags={this.props.hashtag}
+        data-hashtags={this.props.hashtags}
       >
         Tweet
       </a>
@@ -54,6 +46,21 @@ export default class TwitterTweetButton extends React.Component {
   }
 }
 
+TwitterTweetButton.propTypes = {
+  url: React.PropTypes.string,
+  text: React.PropTypes.string,
+  via: React.PropTypes.string,
+  hashtags: React.PropTypes.string,
+  user: React.PropTypes.string
+};
+
+TwitterTweetButton.defaultProps = {
+  url: window.url,
+  text: '',
+  via: '',
+  hashtags: '',
+  user: ''
+};
 /*
 <a href="https://twitter.com/share" class="twitter-share-button" data-url="pageurl" data-text="pagetitle" data-via="uraway_" data-size="large" data-hashtags="hashtag" data-dnt="true">Tweet</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
