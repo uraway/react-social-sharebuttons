0e729bbc79db9a1b6735aca8e70fdade9d9a7bed
uraway <masato.u0107@gmail.com>
2017-01-12 15:59:20 +0900

    fix TumblrFollowButton

--- src/TumblrFollowButton.jsx
+++ src/TumblrFollowButton.jsx
@@ -6,39 +6,30 @@ export default class TumblrFollowButton extends Component {
     this.state = ({ initialized: false });
   }
 
-  componentDidMount() {
-    if (this.state.initialized) {
-      return;
-    }
-
-    this.initialized();
-  }
-
-  initialized() {
-    this.setState({ initialized: true });
-  }
-
-  static PropTypes = {
-    color: PropTypes.string,
-    tumblelog: PropTypes.string,
-  };
-
   render() {
     return (
       <iframe
-        ref="tumblrfollowbutton"
         className="btn"
         frameBorder="0"
-        border="0"
         scrolling="no"
         allowTransparency="true"
         height="20"
         width="65"
-        src={'https://platform.tumblr.com/v2/follow_button.html?type=follow&amp;tumblelog=' + this.props.tumblelog + '&amp;color=' + this.props.color}
+        src={`https://platform.tumblr.com/v2/follow_button.html?type=follow&amp;tumblelog=${this.props.tumblelog}&amp;color=${this.props.color}`}
       />
     );
   }
 }
+
+TumblrFollowButton.propTypes = {
+  color: PropTypes.string,
+  tumblelog: PropTypes.string.isRequired,
+};
+
+TumblrFollowButton.defaultProps = {
+  color: ''
+};
+
 /*
 <iframe class="btn" frameborder="0" border="0" scrolling="no" allowtransparency="true" height="20" width="65" src="https://platform.tumblr.com/v2/follow_button.html?type=follow&amp;tumblelog=staff&amp;color=blue"></iframe>
 */
