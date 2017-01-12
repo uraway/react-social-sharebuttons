c5586d44412493469e8172115f1bb63e79331f05
uraway <masato.u0107@gmail.com>
2017-01-12 15:56:38 +0900

    fix TumblrPostButton

--- src/TumblrPostButton.jsx
+++ src/TumblrPostButton.jsx
@@ -1,5 +1,4 @@
 import React, { Component, PropTypes } from 'react';
-import ReactDOM from 'react-dom';
 
 export default class TumblrPostButton extends Component {
   constructor(props) {
@@ -12,7 +11,7 @@ export default class TumblrPostButton extends Component {
       return;
     }
 
-    const tumblrbutton = ReactDOM.findDOMNode(this.refs.tumblrbutton);
+    const tumblrbutton = this.node;
     const tumblrscript = document.createElement('script');
     tumblrscript.src = 'https://secure.assets.tumblr.com/share-button.js';
     tumblrscript.id = 'tumblr-js';
@@ -25,23 +24,31 @@ export default class TumblrPostButton extends Component {
     this.setState({ initialized: true });
   }
 
-  static PropTypes = {
-    color: PropTypes.string,
-    notes: PropTypes.string,
-  };
-
   render() {
     return (
       <a
-        ref="tumblrbutton"
+        ref={node => this.node = node}
         className="tumblr-share-button"
         data-color={this.props.color}
         data-notes={this.props.notes}
         href="https://embed.tumblr.com/share"
-      />
+      >
+        Tumber Post Button
+      </a>
     );
   }
 }
+
+TumblrPostButton.propTypes = {
+  color: PropTypes.string,
+  notes: PropTypes.string,
+};
+
+TumblrPostButton.defaultProps = {
+  color: '',
+  notes: ''
+};
+
 /*
 <a class="tumblr-share-button" data-color="blue" data-notes="right" href="https://embed.tumblr.com/share"></a>
 <script>!function(d,s,id){var js,ajs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://secure.assets.tumblr.com/share-button.js";ajs.parentNode.insertBefore(js,ajs);}}(document, "script", "tumblr-js");</script>
